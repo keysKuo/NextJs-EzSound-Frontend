d1d13d1afcfc0db7de2dc29ead8f9897df2ec1ff
nKeys Kuo <nkeyskuo124@gmail.com>
2024-05-31 19:25:12 +0700

    add dockerfile

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    server: {
-        port: process.env.PORT || 3000, // Sử dụng cổng được cung cấp bởi biến môi trường PORT hoặc mặc định là 3000
-      },
-};
+const nextConfig = {};
 
 export default nextConfig;
